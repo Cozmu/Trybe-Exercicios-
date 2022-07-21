7db8842e668fd17be95c01e22c57cc01b48c253b
Cozmu <www.jorgereis@gmail.com>
2022-07-21 00:56:01 -0300

    Adicionando dia 1 JavaScript

---
+++ Bloco-4/dia-1-java-script-primeiros-passos/ifElse.js
@@ -0,0 +1,10 @@
+const nota = 75;
+if (nota >= 80){
+    console.log('Parabéns, você foi aprovado(a)!!')
+} 
+else if (nota < 80 && nota >= 60){
+    console.log('Você está na nossa lista de espera')
+}
+else{
+    console.log('Você foi reprovado')
+}
