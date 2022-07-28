9a8816eddcdc6e03ed7469902da479c774ca49b3
Cozmu <www.jorgereis@gmail.com>
2022-07-28 00:10:11 -0300

    Praticando DOM

---
+++ Bloco-5/dia-1-java-script-dom-e-seletores/script.js
@@ -0,0 +1,15 @@
+const tituloPrincipal = document.querySelectorAll('#header-container')[0]
+tituloPrincipal.style.background = 'red';
+
+
+const seiLa = document.querySelector('.emergency-tasks h3')
+seiLa.style.background = 'red';
+
+const nao = document.querySelector('.no-emergency-tasks h3')
+nao.style.background = 'red';
+
+const rodape = document.querySelector('#footer-container')
+rodape.style.background = 'red';
+
+
+
