270c5b0c2d3a9c8436bbe6f24db1035857595f70
Cozmu <www.jorgereis@gmail.com>
2022-08-23 00:06:34 -0300

    Adicionando primeiros dois requisito do exercicio

---
+++ Bloco-8/dia-2-hof-forEach-find-some-every-sort/exercicio.js
@@ -0,0 +1,80 @@
+const books = [
+    {
+        id: 1,
+        name: 'As Crônicas de Gelo e Fogo',
+        genre: 'Fantasia',
+        author: {
+            name: 'George R. R. Martin',
+            birthYear: 1948,
+        },
+        releaseYear: 1991,
+    },
+    {
+        id: 2,
+        name: 'O Senhor dos Anéis',
+        genre: 'Fantasia',
+        author: {
+            name: 'J. R. R. Tolkien',
+            birthYear: 1892,
+        },
+        releaseYear: 1954,
+    },
+    {
+        id: 3,
+        name: 'Fundação',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Isaac Asimov',
+            birthYear: 1920,
+        },
+        releaseYear: 1951,
+    },
+    {
+        id: 4,
+        name: 'Duna',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Frank Herbert',
+            birthYear: 1920,
+        },
+        releaseYear: 1965,
+    },
+    {
+        id: 5,
+        name: 'A Coisa',
+        genre: 'Terror',
+        author: {
+            name: 'Stephen King',
+            birthYear: 1947,
+        },
+        releaseYear: 1986,
+    },
+    {
+        id: 6,
+        name: 'O Chamado de Cthulhu',
+        genre: 'Terror',
+        author: {
+            name: 'H. P. Lovecraft',
+            birthYear: 1890,
+        },
+        releaseYear: 1928,
+    },
+];
+
+//1
+function authorBornIn1947(arr, data) {
+    return arr.find(element => element.author.birthYear === data).author.name;
+}
+console.log(authorBornIn1947(books, 1947));
+//2
+function smallerName(arr) {
+    let nameBook;
+    arr.forEach(element => {
+        if (!nameBook || element.name.length > nameBook.length) {
+            nameBook = element.name;
+        } 
+    });
+    return nameBook;
+}
+console.log(smallerName(books));
+
