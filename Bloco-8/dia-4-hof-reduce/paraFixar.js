9bd349f361bbf8577bb56f4c218b3a14621a06ae
Cozmu <www.jorgereis@gmail.com>
2022-08-24 21:40:33 -0300

    ...

---
+++ Bloco-8/dia-4-hof-reduce/paraFixar.js
@@ -0,0 +1,16 @@
+const numbers = [50, 85, -30, 3, 15];
+
+const getBigger = (bigger, number) => ((bigger > number) ? bigger : number);
+
+const bigger = numbers.reduce(getBigger, 0);
+console.log(bigger); // 85
+//--------------------------------------------------------------------------------------------------------------------------------
+const n = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const getEven = (number) => number % 2 === 0;
+const sumPair = (accumulator, number) => accumulator + number;
+
+const sumNumbers = (array) => array.filter(getEven).reduce(sumPair); // Olhe que código pequeno e conciso!
+
+console.log(sumNumbers(numbers)); // 152
+
