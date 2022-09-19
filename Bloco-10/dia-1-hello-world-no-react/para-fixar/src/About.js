c3975ca2e720ebf2ab89cbfb0b15de8ad00b9a86
Cozmu <www.jorgereis@gmail.com>
2022-09-19 19:23:36 -0300

    para fixar

---
+++ Bloco-10/dia-1-hello-world-no-react/para-fixar/src/About.js
@@ -0,0 +1,17 @@
+
+
+function About() {
+    return (
+        <>
+        <h1>Jorge Welligton</h1>
+        <p>Olá, estou começando a estudar React</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+        </>
+    );
+};
+export default About;
