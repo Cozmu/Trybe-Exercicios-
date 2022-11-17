793662e5d40166811a35bc3c50929ee46d1af67f
Cozmu <www.jorgereis@gmail.com>
2022-11-17 19:42:23 -0300

    dia 1 bloco 15 exercicio fixação

---
+++ Bloco-15/dia-1-reack-hooks/exercicio-de-fixacao/src/App.js
@@ -0,0 +1,94 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [user, setUser] = useState({ name: '', age: '', city: '', modulo: '' });
+
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  return (
+    <form>
+      <label htmlFor="name">
+        NAME:
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={ handleChange }
+          value={ user.name }
+        />
+      </label>
+      <label htmlFor="age">
+        AGE:
+        <input
+          type="number"
+          name="age"
+          id="age"
+          onChange={ handleChange }
+          value={ user.age }
+
+        />
+      </label>
+      <label htmlFor="city">
+        CITY:
+        <input
+          type="text"
+          name="city"
+          id="city"
+          onChange={ handleChange }
+          value={ user.city }
+        />
+      </label>
+      <div className="borda">
+        <label htmlFor="fundamentos">
+          <input
+            id="fundamentos"
+            type="radio"
+            name="modulo"
+            onChange={ handleChange }
+            value="Fundamentos"
+          />
+          Fundamentos
+        </label>
+        <label htmlFor="frontEnd">
+          <input
+            id="frontEnd"
+            type="radio"
+            name="modulo"
+            onChange={ handleChange }
+            value="Front-End"
+          />
+          Front-End
+        </label>
+        <label htmlFor="backEnd">
+          <input
+            id="backEnd"
+            type="radio"
+            name="modulo"
+            onChange={ handleChange }
+            value="Back-End"
+          />
+          Back-End
+        </label>
+        <label htmlFor="cienciaComputacao">
+          <input
+            id="cienciaComputacao"
+            type="radio"
+            name="modulo"
+            onChange={ handleChange }
+            value="Ciências da Computação"
+          />
+          Ciências da Computação
+        </label>
+      </div>
+    </form>
+  );
+}
+
+export default App;
