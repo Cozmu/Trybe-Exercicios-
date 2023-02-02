5f31ba37350d09b7d14586d4ecb85a26e7a6c44b
Cozmu <www.jorgereis@gmail.com>
2023-02-02 18:47:19 -0300

    feat: conteudo dia 02/02

--- Bloco-19/dia-2-servidor-web-com-node/soccer-team-manager/src/app.js
+++ Bloco-19/dia-2-servidor-web-com-node/soccer-team-manager/src/app.js
@@ -1,55 +1,69 @@
 const express = require('express');
+require('express-async-errors'); 
+const validateTeam = require('./middlewares/validateTeam');
+const existingId = require('./middlewares/existingId');
+const apiCredentials = require('./middlewares/apiCredentials');
 
 const app = express();
-app.use(express.json());
 
+let nextId = 3;
 const teams = [
-    {
-      id: 1,
-      name: 'São Paulo Futebol Clube',
-      initials: 'SPF',
-    },
-    {
-      id: 2,
-      name: 'Clube Atlético Mineiro',
-      initials: 'CAM',
-    },
+  { id: 1, nome: 'São Paulo Futebol Clube', sigla: 'SPF' },
+  { id: 2, nome: 'Sociedade Esportiva Palmeiras', sigla: 'PAL' },
 ];
 
-app.get('/teams', (req, res) => res.status(200).json({ teams }));
+app.use(apiCredentials);
+app.use(express.json());
 
-app.post('/teams', (req, res) => {
-    const newTeam = { ...req.body };
-    teams.push(newTeam);
-  
-    res.status(201).json({ team: newTeam });
+app.get('/teams', (req, res) => res.json(teams));
+
+app.get('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+  if (team) {
+    res.json(team);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
-app.put('/teams/:id', (req, res) => {
-    const { id } = req.params;
-    const { name, initials } = req.body;
-  
-    const updateTeam = teams.find((team) => team.id === Number(id));
-  
-    if (!updateTeam) {
-      res.status(404).json({ message: 'Team not found' });
-    }
-  
-    updateTeam.name = name;
-    updateTeam.initials = initials;
-    res.status(200).json({ updateTeam });
+app.post('/teams', validateTeam, (req, res) => {
+  if (
+    !req.teams.teams.includes(req.body.sigla)
+    && teams.every((t) => t.sigla !== req.body.sigla)
+  ) {
+    return res.status(422).json({ message: 'Já existe um time com essa sigla' });
+  }
+  const team = { id: nextId, ...req.body };
+  teams.push(team);
+  nextId += 1;
+  res.status(201).json(team);
 });
 
-app.delete('/teams/:id', (req, res) => {
-    const { id } = req.params;
-    const arrayPosition = teams.findIndex((team) => team.id === Number(id));
-    teams.splice(arrayPosition, 1);
-  
-    res.status(200).end();
+app.put('/teams/:id', validateTeam, existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+  if (team) {
+    const index = teams.indexOf(team);
+    const updated = { id, ...req.body };
+    teams.splice(index, 1, updated);
+    res.status(201).json(updated);
+  } else {
+    res.sendStatus(400);
+  }
 });
 
-app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+app.delete('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+  if (team) {
+    const index = teams.indexOf(team);
+    teams.splice(index, 1);
+  }
+  res.sendStatus(204);
+});
 
-module.exports = app;
-
-// const OK = 200, const INTERNAL_SERVER_ERROR = 500 e const NOT_FOUND = 404 
+module.exports = {
+  app,
+  teams,
+};
