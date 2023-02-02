946f554eb6e653f1317952bb967e0ddfa2ead796
Cozmu <www.jorgereis@gmail.com>
2023-02-02 19:31:19 -0300

    build: instalando e organizando dependencias

---
+++ Bloco-19/dia-4-express-middlewares/src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.post('/activities', (req, res) => {
+    res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
+}); 
+
+module.exports = app;
