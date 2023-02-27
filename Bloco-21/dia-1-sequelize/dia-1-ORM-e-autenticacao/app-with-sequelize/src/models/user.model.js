b93bc5f7bc1cd9f18ab411f666b5d63aa538ba07
Cozmu <www.jorgereis@gmail.com>
2023-02-27 17:00:24 -0300

    feat: add conteudo de hj

--- Bloco-21/dia-1-sequelize/dia-1-ORM-e-autenticacao/app-with-sequelize/src/models/user.model.js
+++ Bloco-21/dia-1-sequelize/dia-1-ORM-e-autenticacao/app-with-sequelize/src/models/user.model.js
@@ -2,11 +2,34 @@ const UserModel = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     fullName: DataTypes.STRING,
     email: DataTypes.STRING,
+    phoneNum: DataTypes.STRING, 
   });
   
   (async () => {
     await sequelize.sync({ force: true });
-    // As funções vão aqui
+    // const sara = User.build({
+    //   fullName: 'Sara Silva Santos',
+    //   email: 'sara.ss@trybe.com',
+    // });
+    // await sara.save();
+
+    // const sara = await User.create({ // PARA CRIAR
+    //   fullName: 'Sara Silva Santos',
+    //   email: 'sara.ss@trybe.com',
+    // });
+
+    // const jane = await User.create({ 
+    //   fullName: "Jane Doe",
+    //   email: "jane.doe@trybe.com",
+    // });
+    // jane.email = "ada.doe@trybe.com";
+    // await jane.update({ fullName: "Ada Joe" }); // PARA ATUALIZAR
+    // await jane.save();
+
+    // const mario = await User.create({ fullName: "Mario Bors" }); // PARA EXCLUIR
+    // await mario.destroy();
+
+
   })();
 
   return User;
