386ef3a1e9a0a2a681dd9e25b0dd316fc08b5c84
Cozmu <www.jorgereis@gmail.com>
2023-03-20 20:45:22 -0300

    feat: ex 4, 5

--- Bloco-24/dia-1-introducao-orientacao-a-obj/exercicio.ts
+++ Bloco-24/dia-1-introducao-orientacao-a-obj/exercicio.ts
@@ -118,10 +118,92 @@ const aluno1 = new Student('aaaaaaaaaaaaaaaaaaaaaaa', [10, 8, 2, 3], [3, 2], new
 console.log(aluno1);
 
 interface Employee {
-  registration:number,
+  registration:string,
   salary:number,
   admissionDate:Date,
   generateRegistration():string,
+}
+
+class Subject {
+  constructor(
+    private _name:string,
+  ) {
+    this.name = _name
+  }
+
+  get listName():string {
+    return this._name;
+  }
+
+  private validateName(name:string):void {
+    if (name.length < 3) {
+      throw new Error('O nome tem que possuir no mínimo 3 caracteres');
+    };
+  }
+
+  set name(value:string) {
+    this.validateName(value);
+    this._name = value;
+  }
+}
+
+export default class Teacher extends Person implements Employee {
+  private _subject: Subject;
+  private _registration = String();
+  private _admissionDate: Date;
+
+  constructor(name: string, birthDate: Date, private _salary: number, subject: Subject) {
+    super(name, birthDate);
+
+    this._subject = subject;
+    this.salary = _salary;
+    this._admissionDate = new Date();
+    this.registration = this.generateRegistration();
+  }
+
+  get subject(): Subject {
+    return this._subject;
+  }
+
+  set subject(value: Subject) {
+    this._subject = value;
+  }
+
+  get listRegistration(): string {
+    return this._registration;
+  }
+
+  set registration(value: string) {
+    if (value.length < 16) throw new Error('O registro deve possuir no mínimo 16 caracteres.');
+
+    this._registration = value;
+  }
+
+  get salary(): number {
+    return this._salary;
+  }
+
+  set salary(value: number) {
+    if (value < 0) throw new Error('O salário não pode ser negativo.');
+
+    this._salary = value;
+  }
+
+  get admissionDate(): Date {
+    return this._admissionDate;
+  }
+
+  set admissionDate(value: Date) {
+    if (value.getTime() > new Date().getTime()) throw new Error('A data de admissão não pode ser uma data no futuro.');
+
+    this._admissionDate = value;
+  }
+
+  generateRegistration(): string {
+    const randomStr = String(Date.now() * (Math.random() + 1)).replace(/\W/g, '');
+
+    return `PRF${randomStr}`;
+  }
 }
 
 
