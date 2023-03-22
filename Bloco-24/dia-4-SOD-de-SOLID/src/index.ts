f06483064c73cd50a25c8640daf551127229b900
Cozmu <www.jorgereis@gmail.com>
2023-03-22 16:21:58 -0300

    feat: conteudo

---
+++ Bloco-24/dia-4-SOD-de-SOLID/src/index.ts
@@ -0,0 +1,94 @@
+type Discipline = {
+  name: string;
+  grade: number;
+  letterGrade?: string;
+};
+
+type School = {
+  name: string;
+  approvalGrade: number;
+};
+
+type Student = {
+  name: string;
+  disciplines: Discipline[];
+  school: School;
+};
+
+/* Apoio para a função `getGradeLetter` */
+const GRADE_DICT = {
+  numbers: [0.9, 0.8, 0.7, 0.6, 0.1],
+  letters: ['A', 'B', 'C', 'D', 'E'],
+};
+
+/* Função menor para remover o uso excessivo de `if{}else`s */
+const getGradeLetter = (gradeNumber: number): string => {
+  const gradeNumbers = GRADE_DICT.numbers;
+  const gradeLetters = GRADE_DICT.letters;
+  for (let i = 0; i < gradeNumbers.length; i += 1) {
+    if (gradeNumber >= gradeNumbers[i]) return gradeLetters[i];
+  }
+  return 'F';
+};
+
+/* Coletar notas */
+const getLetterGrades = (discipline: Discipline): Discipline => ({
+  ...discipline,
+  letterGrade: getGradeLetter(discipline.grade),
+});
+
+/* "Converter" */
+const percentageGradesIntoLetters = (student: Student): Student => ({
+  ...student,
+  disciplines: student.disciplines.map(getLetterGrades),
+});
+
+/* "Determinar" */
+const approvedStudents = ({ disciplines, school }: Student): boolean =>
+  disciplines.every(({ grade }) => grade >= school.approvalGrade);
+
+/* "Atualizar" */
+const updateApprovalData = (student: Student): void => {
+  console.log(`A pessoa com nome ${student.name} foi aprovada!`);
+
+  student.disciplines.forEach(({ name, letterGrade }) =>
+    console.log(`${name}: ${letterGrade}`));
+};
+
+function setApproved(students: Student[]): void {
+  students
+    .map(percentageGradesIntoLetters)
+    .filter(approvedStudents) 
+    /* como o proximo map ta pegando os elementos se esse 
+    filter ta com um every dentro e so retorna booleano ?? */
+    .map(updateApprovalData);
+}
+
+const studentsExample = [
+  {
+    name: 'Lee',
+    school: { name: 'Standard', approvalGrade: 0.7 },
+    disciplines: [
+      { name: 'matemática', grade: 0.8 },
+      { name: 'história', grade: 0.9 },
+    ],
+  },
+  {
+    name: 'Albus',
+    school: { name: 'Hogwarts', approvalGrade: 0.8 },
+    disciplines: [
+      { name: 'divination', grade: 0.8 },
+      { name: 'potions', grade: 0.9 },
+    ],
+  },
+];
+
+setApproved(studentsExample);
+
+export {
+  percentageGradesIntoLetters,
+  approvedStudents,
+  updateApprovalData,
+  setApproved,
+  getLetterGrades,
+};
