1f4a2792f0804abf1aa70ada84d81706cf15acdf
Cozmu <www.jorgereis@gmail.com>
2023-05-30 21:36:41 -0300

    feat: exercicio

---
+++ Bloco-28/dia-2-recursividade/exercicio.py
@@ -0,0 +1,38 @@
+def conta_pares(n):
+    numero_de_pares = 0
+    for num in range(n+1):
+        if num % 2 == 0 and num != 0:
+            numero_de_pares += 1
+    return numero_de_pares
+
+def conta_pares(n):
+    if n == 1:
+        return 0
+    elif n % 2 == 0:
+        return 1 + conta_pares(n-1)
+    else:
+        return conta_pares(n-1)
+
+
+def maiorinteiro_aux(lista, tamanho):
+    if tamanho == 1:
+        return lista[0]
+    else:
+        maior_do_resto_da_lista = maiorinteiro_aux(lista, tamanho-1)
+        if maior_do_resto_da_lista > lista[tamanho-1]:
+            return maior_do_resto_da_lista
+        else:
+            return lista[tamanho-1]
+
+
+def maiorinteiro(lista):
+    tamanho = len(lista)
+    return maiorinteiro_aux(lista, tamanho)
+
+
+print(maiorinteiro([1, 21, 300, 4, 57]))
+
+def mdc(a, b):
+    if b == 0:
+        return a
+    return mdc(b, a % b)
