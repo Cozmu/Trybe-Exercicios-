b24d5716467799e0ba50d0b1a0026c3e5b0ef624
Cozmu <www.jorgereis@gmail.com>
2023-06-07 16:02:07 -0300

    feat: conteudo

---
+++ Bloco-29/dia-3-no-listas-encadeadas/linked_list_content.py
@@ -0,0 +1,100 @@
+from node import Node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head_value = None
+        self.__length = 0
+
+    def __str__(self):
+        return f"LinkedList(len={self.__length}, value={self.head_value})"
+
+    def __len__(self):
+        return self.__length
+    
+    def insert_first(self, value):
+        first_value = Node(value)
+        first_value.next = self.head_value
+        self.head_value = first_value
+        self.__length += 1
+    
+    def insert_last(self, value):
+        last_value = Node(value)
+        current_value = self.head_value
+
+        if current_value is None:
+            return self.insert_first(value)
+
+        while current_value.next:
+            current_value = current_value.next
+        current_value.next = last_value
+        self.__length += 1
+
+
+    def insert_at(self, value, position):
+        if position < 1:
+            return self.insert_first(value)
+        if position >= len(self):
+            return self.insert_last(value)
+        current_value = self.head_value
+        while position > 1:
+            current_value = current_value.next
+            position -= 1
+        next_value = Node(value)
+        next_value.next = current_value.next
+        current_value.next = next_value
+        self.__length += 1
+    
+    def remove_first(self):
+        value_to_be_removed = self.head_value
+        if value_to_be_removed:
+            self.head_value = self.head_value.next
+            value_to_be_removed.next = None
+            self.__length -= 1
+        return value_to_be_removed
+
+    def remove_last(self):
+        if len(self) <= 1:
+            return self.remove_first()
+
+        previous_to_be_removed = self.head_value
+
+        while previous_to_be_removed.next.next:
+            previous_to_be_removed = previous_to_be_removed.next
+
+        value_to_be_removed = previous_to_be_removed.next
+        previous_to_be_removed.next = None
+        self.__length -= 1
+        return value_to_be_removed
+    
+    def remove_at(self, position):
+        if position < 1:
+            return self.remove_first()
+        if position >= len(self):
+            return self.remove_last()
+
+        previous_to_be_removed = self.head_value
+        while position > 1:
+            previous_to_be_removed = previous_to_be_removed.next
+            position -= 1
+        value_to_be_removed = previous_to_be_removed.next
+        previous_to_be_removed.next = value_to_be_removed.next
+        value_to_be_removed.next = None
+        self.__length -= 1
+        return value_to_be_removed
+    
+    def get_element_at(self, position):
+        value_returned = None
+        value_to_be_returned = self.head_value
+        if value_to_be_returned:
+            while position > 0 and value_to_be_returned.next:
+                value_to_be_returned = value_to_be_returned.next
+                position -= 1
+            if value_to_be_returned:
+                value_returned = Node(value_to_be_returned.value)
+        return value_returned
+    
+    def is_empty(self):
+        return not self.__length
+
+# LinkedList(len=1 value=Node(value=3 next=None))
